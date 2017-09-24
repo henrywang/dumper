f07cfa50fb5b78f9bf1999b39813c16f5f419fa7
Xiaofeng Wang <henrywangxf@me.com>
2017-09-24 22:14:58 +0800

    Add stacker API support.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const stacker = require('./controllers/stacker');
 
 const app = express();
+
+app.use('/api/v0/stacker', stacker);
 
 module.exports = app;
--- controllers/stacker.js
+++ controllers/stacker.js
@@ -4,4 +4,12 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 router.use(bodyParser.urlencoded({ extended: true }));
 
+router.get('/ipaddrs', (req, resp) => {
+  resp.send('stacker get api.')
+})
+
+router.post('/ipaddrs', (req, resp) => {
+  resp.send('stacker post api.')
+})
+
 module.exports = router;
