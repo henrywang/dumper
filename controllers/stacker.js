614985615682064cc3d4b38d0c3e0a5259041567
Xiaofeng Wang <xiaofwan@dhcp-14-109.nay.redhat.com>
2017-09-26 17:10:35 +0800

    Do not use mongoose any more but use mongodb instead.

--- controllers/stacker.js
+++ controllers/stacker.js
@@ -2,14 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const router = express.Router();
-router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
 
-router.get('/ipaddrs', (req, resp) => {
-  resp.send('stacker get api.')
+router.get('/ipaddrs/:vm_name', (req, res) => {
+  if (req.accepts('application/json')) {
+    res.set('Content-Type', 'application/json; charset=utf-8')
+       .json(req.params)
+  } else {
+    res.status(406)
+       .send('Not Acceptable');
+  }
+
 })
 
-router.post('/ipaddrs', (req, resp) => {
-  resp.send('stacker post api.')
+router.post('/ipaddrs', (req, res) => {
+  res.json(req.body)
 })
 
 module.exports = router;
