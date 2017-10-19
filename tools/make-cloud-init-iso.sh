a93715ae055710dc32883750cd6a699e24849cdf
Xiaofeng Wang <henrywangxf@me.com>
2017-10-19 12:47:15 +0800

    Fix runcmd script issue by make them base64 to hide some special characters which script will use.

--- tools/make-cloud-init-iso.sh
+++ tools/make-cloud-init-iso.sh
@@ -16,6 +16,7 @@ key_pub=$6
 init_dir=$(mktemp -d)
 meta_data="$init_dir/meta-data"
 user_data="$init_dir/user-data"
+upload_script="$init_dir/upload_script"
 iso_image="cloud-init.iso"
 log_file=$(mktemp -t runcmd.log.XXXXXXXXXX)
 
@@ -23,6 +24,14 @@ vm_user="root"
 vm_pass="atomic"
 
 mkdir -p $init_dir
+
+cat >$upload_script <<EOF
+#!/bin/bash
+curl --connect-timeout 2 --max-time 3 -i -X POST -H 'Content-Type: application/json' -d '{"vmName": "Atomic-${version}-${hypervisor}-${firmware}-${build_num}", "ip": "'"VMIPCMD"'"}' ${api_url} >${log_file}
+exit 0
+EOF
+sed -i $upload_script -e 's/VMIPCMD/$(ip route get 8.8.8.8 | head -1 | cut -d\x27\ \x27 -f7)/'
+script_b64=`cat $upload_script | base64 -w 0`
 
 cat >$meta_data <<EOF
 EOF
@@ -33,16 +42,21 @@ users:
   - default
   - name: ${vm_user}
     groups: users,wheel
-    ssh_authorized_keys:
+    ssh-authorized-keys:
       - ${key_pub}
 ssh_pwauth: True
 chpasswd:
   list: |
     ${vm_user}:${vm_pass}
   expire: False
+write_files:
+  - encoding: b64
+    path: /var/lib/cloud/upload_ip.sh
+    permissions: '0555'
+    content: ${script_b64}
+
 runcmd:
-  - curl --connect-timeout 2 --max-time 3 -i -X POST -H 'Content-Type: application/json' -d '{"vmName": "Atomic-${version}-${hypervisor}-${firmware}-${build_num}", "ip": "'"VMIPCMD"'"}' ${api_url} >${log_file}
+  - /var/lib/cloud/upload_ip.sh
 EOF
 
-sed -i $user_data -e 's/VMIPCMD/$(ip route get 8.8.8.8 | head -1 | cut -d\x27\ \x27 -f7)/'
 genisoimage -input-charset utf-8 -output $iso_image -volid cidata -joliet -rock $user_data $meta_data
