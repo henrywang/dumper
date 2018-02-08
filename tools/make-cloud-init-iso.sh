f48588caa3db1cf08d6884517d3117fedc85b38b
Xiaofeng Wang <henrywangxf@me.com>
2018-02-08 13:00:32 +0800

    Remove complex parameters, use vm_name instead.

--- tools/make-cloud-init-iso.sh
+++ tools/make-cloud-init-iso.sh
@@ -6,11 +6,8 @@
 set -e
 
 api_url=$1
-version=$2
-hypervisor=$3
-firmware=$4
-build_num=$5
-key_pub=$6
+vm_name=$2
+key_pub=$3
 
 # create the cloud-init iso
 init_dir=$(mktemp -d)
@@ -27,7 +24,7 @@ mkdir -p $init_dir
 
 cat >$upload_script <<EOF
 #!/bin/bash
-curl --connect-timeout 2 --max-time 3 -i -X POST -H 'Content-Type: application/json' -d '{"vmName": "Atomic-${version}-${hypervisor}-${firmware}-${build_num}", "ip": "'"VMIPCMD"'"}' ${api_url} >${log_file}
+curl --connect-timeout 2 --max-time 3 -i -X POST -H 'Content-Type: application/json' -d '{"vmName": "${vm_name}", "ip": "'"VMIPCMD"'"}' ${api_url} >${log_file}
 exit 0
 EOF
 sed -i $upload_script -e 's/VMIPCMD/$(ip route get 8.8.8.8 | head -1 | cut -d\x27\ \x27 -f7)/'
